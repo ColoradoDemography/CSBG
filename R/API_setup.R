6e2162e0ae8a5b3e53c14ec37ba2c536f65bcd7c
ABSOD <32849428+ABSOD@users.noreply.github.com>
2020-01-17 10:14:29 -0700

    Update API_setup.R

--- R/API_setup.R
+++ R/API_setup.R
@@ -1,7 +1,7 @@
 #  APIS_setup:  Contains data specific values for ACS data and API Keys
 
 # Current ACS database
-curACS <- "acs1317"
-curYr <- 2017
+curACS <- "acs1418"
+curYr <- 2018
 fipslist <<- ""
 censAPI <- "08fe07c2a7bf781b7771d7cccb264fe7ff8965ce"
