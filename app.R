b814f95655a44cfe51f53e601d7bc8e52954f1e3
ABSOD <32849428+ABSOD@users.noreply.github.com>
2022-10-10 09:51:04 -0600

    Version 2.1

--- app.R
+++ app.R
@@ -58,7 +58,7 @@ source("R/boxContent.R")
 source("R/captionSrc.R")
 source("R/chkID.R")
 source("R/clrGeoname.R")
-
+source("R/genDropdown.R")
 source("R/CountyName.R")
 source("R/disabilityPRO.R")
 source("R/downloadObj.R")
@@ -96,10 +96,10 @@ source("R/pctMOE.R")
 # The GLOBAL Variables  Add Additional lists items as sections get defined
 #File Locations ALSO LOOK AT LINE IN THE WORD OUTPUT CODE  LINE 990
 # Local/Development
-tPath <- "C:/Users/abickford/Documents/TempDir"  #Development
+#tPath <- "C:/Users/abickford/Documents/TempDir"  #Development
  
 #Production
-# tPath <- "/tmp"  
+ tPath <- "/tmp"  
 
 # Locations for Google Analtyics Java Script Files
 # Local/ Development
