f78b959d9e3870c428fca3463ccb6999d8f0dcac
ABSOD <32849428+ABSOD@users.noreply.github.com>
2024-08-06 08:38:31 -0600

    Add files via upload

--- R/API_setup.R
+++ R/API_setup.R
@@ -1,7 +1,7 @@
 #  APIS_setup:  Contains data specific values for ACS data and API Keys
 
 # Current ACS database
-curACS <- "acs1721"
-curYr <- 2021
+curACS <- "acs1822"
+curYr <- 2022
 fipslist <<- ""
 censAPI <- "08fe07c2a7bf781b7771d7cccb264fe7ff8965ce"
